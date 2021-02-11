0bfef27eb28681704119b20582c59c2edaf6c0d2
mikhail <sharashin.m@avtocod.ru>
2021-02-11 18:35:32 +0500

    ex 3.9, 3.10, 3.11

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,4 @@
 fun main(args: Array<String>) {
-	Exercises
 	testables.forEach { it.invokeTest() }
 }
 val testables = mutableListOf<Testable>()
@@ -60,6 +59,23 @@ object Exercises {
 			return f(2) == "20"
 		}
 	}
+	val ex_3_9 = object : Testable() {
+		fun <A, B, C, D> funcFrom(a: A, b: B, c: C, d: D): String = "$a $b $c $d"
+		fun <A, B, C, D> funcToCurried(): (A) -> (B) -> (C) -> (D) -> String = {a -> {b -> {c -> {d -> "$a $b $c $d" }}}}
+		override fun test(): Boolean {
+			return funcFrom(1, 2L, 3.0, "4") == funcToCurried<Int, Long, Double, String>()(1)(2L)(3.0)("4")
+		}
+	}
+	val ex_3_10 = object : Testable() {
+		fun <A, B, C> curried(f: (A, B) -> C): (A)->(B)->C = { a -> {b -> f(a, b) }}
+		override fun test(): Boolean = curried { a: Int, b: Double -> a + b }(2)(3.6) == 7.2
+	}
+	val ex_3_11 = object : Testable() {
+		val addTax: (Int) -> (Int) -> Int = { x -> { y -> x * 10 + y }}
+		fun <A, B, C> swapArgs(f: (A) -> (B) -> C): (B) -> (A) -> C = { b -> { a -> f(a)(b) }}
+		override fun test(): Boolean = addTax(1)(2) == 12 && swapArgs(addTax)(2)(1) == 12
+	}
+
 }
 
 
