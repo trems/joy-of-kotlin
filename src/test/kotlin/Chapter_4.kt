a0017b0399b3fa94a2547fbc0e597348575609b1
trems <trems95@gmail.com>
2021-02-14 00:16:44 +0500

    ex 4.3

--- src/test/kotlin/Chapter_4.kt
+++ src/test/kotlin/Chapter_4.kt
@@ -1,5 +1,7 @@
+import io.kotest.assertions.throwables.shouldNotThrowAnyUnit
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.math.BigInteger
 
 class Chapter_4 : StringSpec({
     "ex 4.1" {
@@ -15,5 +17,16 @@ class Chapter_4 : StringSpec({
         test.factorial(0) shouldBe 1
         test.factorial(2) shouldBe 2
         test.factorial(4) shouldBe 24
+    }
+    "ex 4.3" {
+        fun fib(n: Int): BigInteger {
+            tailrec fun process(prev1: BigInteger, prev2: BigInteger, current: Int): BigInteger {
+                return if (current == n) prev1 + prev2
+                else process(prev1 + prev2, prev1, current + 1)
+            }
+            return if (n < 2) BigInteger.ONE else process(BigInteger.ONE, BigInteger.ONE, 2)
+        }
+        (0..10).map { fib(it) } shouldBe (listOf(1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89).map { BigInteger.valueOf(it.toLong()) })
+        shouldNotThrowAnyUnit { fib(20_000) } // verify tailrec is working
     }
 })
